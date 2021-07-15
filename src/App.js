0452136b4896a52aac8245b61f8b85cbececf1d7
Jacky Lee <dr.jackylee@gmail.com>
2021-07-15 00:04:18 -0700

    Add bootstrap

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>Food Tracker</div>;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/styles.css";
 
-const App = () => <div>Food Tracker</div>;
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
